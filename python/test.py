3707f701c0e4111ca2cfca2ce4e8e14768a72b68
laundrevity <mahany25@gmail.com>
2023-04-21 19:45:50 +0200

    added dynamic step size, validated very simple case

--- python/test.py
+++ python/test.py
@@ -1,7 +1,25 @@
 import convex
 import numpy as np
+import time
 
 
 if __name__ == '__main__':
-    xs = np.zeros(8)
-    print(convex.optimize)
+    Q = np.array([[1, 0], [0, 1]]).flatten()
+    p = np.array([0, 0])
+    b = np.array([1, 1])
+    s = np.array([1, 1])
+    a = np.array([1, 1])
+    E = 1
+    x0 = np.array([0.0, 0.0])
+
+    # dynamic step size params
+    alpha = 1.0
+    gamma = 1.0
+    max_iter = 25
+
+    t0 = time.time()
+    x_star = convex.optimize(Q, p, b, s, x0, max_iter, a, E, alpha, gamma)
+    t1 = time.time()
+
+    print(f"x_star: {x_star}")
+    print(f'calculation time: {(t1-t0)*1000:.2f} ms')
--- src/lib.rs
+++ src/lib.rs
@@ -18,18 +18,19 @@ pub fn projected_subgradient_descent(
     s: &DVector<f64>,
     x0: &DVector<f64>,
     max_iter: usize,
-    step_size: f64,
     a: &DVector<f64>,
     e: f64,
+    alpha: f64,
+    gamma: f64,
 ) -> DVector<f64> {
     let mut x = x0.clone();
 
-    for _ in 0..max_iter {
+    for iter in 0..max_iter {
         let grad_f = 2.0 * Q * &x + p;
         let subgrad_g = max_elementwise(b, s);
         let subgrad = grad_f + subgrad_g;
-
-        x = x - step_size * subgrad;
+        let dynamic_step_size = alpha / (gamma + iter as f64);
+        x = x - dynamic_step_size * subgrad;
         x = project_onto_hyperplane(&x, a, e);
     }
 
@@ -47,9 +48,10 @@ fn optimize(
     s: Vec<f64>,
     x0: Vec<f64>,
     max_iter: usize,
-    step_size: f64,
     a: Vec<f64>,
     e: f64,
+    alpha: f64,
+    gamma: f64,
 ) -> PyResult<Vec<f64>> {
     let n = x0.len();
     let Q = DMatrix::from_row_slice(n, n, &Q);
@@ -59,7 +61,7 @@ fn optimize(
     let x0 = DVector::from_vec(x0);
     let a = DVector::from_vec(a);
 
-    let result = projected_subgradient_descent(&Q, &p, &b, &s, &x0, max_iter, step_size, &a, e);
+    let result = projected_subgradient_descent(&Q, &p, &b, &s, &x0, max_iter, &a, e, alpha, gamma);
 
     Ok(result.iter().cloned().collect::<Vec<f64>>())
 }
