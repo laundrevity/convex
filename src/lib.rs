f45d02669d81fe0bd021bdbc7676da420953c770
laundrevity <mahany25@gmail.com>
2023-04-21 19:16:48 +0200

    renamed variables, fixed GPT4's other math mistake

--- src/lib.rs
+++ src/lib.rs
@@ -12,64 +12,54 @@ fn project_onto_hyperplane(x: &DVector<f64>, a: &DVector<f64>, e: f64) -> DVecto
 }
 
 pub fn projected_subgradient_descent(
-    a: &DMatrix<f64>,
-    q: &DVector<f64>,
+    Q: &DMatrix<f64>,
+    p: &DVector<f64>,
     b: &DVector<f64>,
     s: &DVector<f64>,
     x0: &DVector<f64>,
     max_iter: usize,
     step_size: f64,
-    a_hyperplane: &DVector<f64>,
+    a: &DVector<f64>,
     e: f64,
 ) -> DVector<f64> {
     let mut x = x0.clone();
 
     for _ in 0..max_iter {
-        let grad_f = a * &x + q;
+        let grad_f = 2.0 * Q * &x + p;
         let subgrad_g = max_elementwise(b, s);
         let subgrad = grad_f + subgrad_g;
 
         x = x - step_size * subgrad;
-        x = project_onto_hyperplane(&x, a_hyperplane, e);
+        x = project_onto_hyperplane(&x, a, e);
     }
 
     x
 }
 
 /// Gives an approximate solution to convex optimization functions of the form
-/// f(x) = x^T A x + q^T x + Σ max(x_i b_i, x_i s_i), such that a^T x = E,
-/// given matrix A, vectors q, b, s, a, and real E
+/// f(x) = x^T Q x + p^T x + Σ max(x_i b_i, x_i s_i), such that a^T x = E,
+/// given symmetric matrix Q, vectors q, b, s, a, and real E
 #[pyfunction]
 fn optimize(
-    a_matrix: Vec<f64>,
-    q_vector: Vec<f64>,
-    b_vector: Vec<f64>,
-    s_vector: Vec<f64>,
+    Q: Vec<f64>,
+    p: Vec<f64>,
+    b: Vec<f64>,
+    s: Vec<f64>,
     x0: Vec<f64>,
     max_iter: usize,
     step_size: f64,
-    a_hyperplane: Vec<f64>,
+    a: Vec<f64>,
     e: f64,
 ) -> PyResult<Vec<f64>> {
     let n = x0.len();
-    let a_matrix = DMatrix::from_row_slice(n, n, &a_matrix);
-    let q_vector = DVector::from_vec(q_vector);
-    let b_vector = DVector::from_vec(b_vector);
-    let s_vector = DVector::from_vec(s_vector);
-    let x0_vector = DVector::from_vec(x0);
-    let a_hyperplane_vector = DVector::from_vec(a_hyperplane);
+    let Q = DMatrix::from_row_slice(n, n, &Q);
+    let p = DVector::from_vec(p);
+    let b = DVector::from_vec(b);
+    let s = DVector::from_vec(s);
+    let x0 = DVector::from_vec(x0);
+    let a = DVector::from_vec(a);
 
-    let result = projected_subgradient_descent(
-        &a_matrix,
-        &q_vector,
-        &b_vector,
-        &s_vector,
-        &x0_vector,
-        max_iter,
-        step_size,
-        &a_hyperplane_vector,
-        e,
-    );
+    let result = projected_subgradient_descent(&Q, &p, &b, &s, &x0, max_iter, step_size, &a, e);
 
     Ok(result.iter().cloned().collect::<Vec<f64>>())
 }
